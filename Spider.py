4115af48675f4e89f309a1b39952e3a18426f3ea
SibtealiBaqar <sibteali786@gmail.com>
2020-10-31 19:08:58 +0500

    Added ssl certification errors repair and created tables for data base

--- Spider.py
+++ Spider.py
@@ -1,1 +1,26 @@
+import sqlite3
+import urllib.error
+import ssl
+from urllib.parse import urljoin
+from urllib.parse import urlparse
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS Pages
+    (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT,
+     error INTEGER, old_rank REAL, new_rank REAL)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS Links
+    (from_id INTEGER, to_id INTEGER)''')
+
+cur.execute('''CREATE TABLE IF NOT EXISTS Webs (url TEXT UNIQUE)''')
+
 
