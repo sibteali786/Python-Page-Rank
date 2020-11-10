7f1a345f7359f569bf2e3eaeeb75773d066a4f41
SibtealiBaqar <sibteali786@gmail.com>
2020-11-10 22:34:16 +0500

    Removed Errors in Sprank.py and redesigned its code to update database after evaluating page_rank

--- Sprank.py
+++ Sprank.py
@@ -48,41 +48,60 @@ if len(prev_ranks) < 1:
 # ranking a page in Memory helps us fast the process to retreive and use a data with copying actual data
 """ Applying Page rank Algorithm """
 for i in range(many):
-
-    next_ranks = dict()
+    # print prev_ranks.items()[:5]
+    next_ranks = dict();
     total = 0.0
     for (node, old_rank) in list(prev_ranks.items()):
         total = total + old_rank
         next_ranks[node] = 0.0
+    # print total
 
     # Find the number of outbound links and sent the page rank down each
     for (node, old_rank) in list(prev_ranks.items()):
+        # print node, old_rank
         give_ids = list()
         for (from_id, to_id) in links:
             if from_id != node: continue
-            # print(from_id, to_id)
+            #  print '   ',from_id,to_id
+
             if to_id not in to_ids: continue
-
             give_ids.append(to_id)
-            if (len(give_ids) < 1): continue
-            # print(give_ids)
-            amount = old_rank/len(give_ids)     # It is a rank divided by the number of outgoing links it has
-            # print(old_rank ,amount)
-            for id in give_ids:
-                next_ranks[id] = next_ranks[id] + amount
-                # print(next_ranks)
-        new_total = 0
-        for (node, next_rank) in list(next_ranks.items()):
-            new_total = new_total + next_rank
-            # print(next_ranks)
-        evap = (total - new_total)/len(next_ranks)
-        # print(evap)   # Decreases progressively
+        if (len(give_ids) < 1): continue
+        amount = old_rank / len(give_ids)
+        # print node, old_rank,amount, give_ids
 
-        for node in next_ranks:
-            next_ranks[node] = next_ranks[node] + evap
+        for id in give_ids:
+            next_ranks[id] = next_ranks[id] + amount
 
-        newtot = 0
-        for (node, next_rank) in list(next_ranks.items()):
-            newtot = newtot + next_rank
+    newtot = 0
+    for (node, next_rank) in list(next_ranks.items()):
+        newtot = newtot + next_rank
+    evap = (total - newtot) / len(next_ranks)
 
+    # print newtot, evap
+    for node in next_ranks:
+        next_ranks[node] = next_ranks[node] + evap
 
+    newtot = 0
+    for (node, next_rank) in list(next_ranks.items()):
+        newtot = newtot + next_rank
+
+    totdiff = 0
+    for (node, old_rank) in list(prev_ranks.items()):
+        new_rank = next_ranks[node]
+        diff = abs(old_rank - new_rank)
+        totdiff = totdiff + diff
+
+    avediff = totdiff / len(prev_ranks)
+    print(i + 1, avediff)
+
+    # rotate
+    prev_ranks = next_ranks
+
+# Put the final ranks back into the database
+print(list(next_ranks.items())[:5])
+cur.execute('''UPDATE Pages SET old_rank=new_rank''')
+for (id, new_rank) in list(next_ranks.items()):
+    cur.execute('''UPDATE Pages SET new_rank=? WHERE id=?''', (new_rank, id))
+conn.commit()
+cur.close()
